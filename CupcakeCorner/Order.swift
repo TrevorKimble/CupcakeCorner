44bf6372f2067a4b99c6e725856a1251fc636163
TrevorKimble <117454217+TrevorKimble@users.noreply.github.com>
2024-04-03 13:36:32 -0400

    Add files via upload

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,64 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Trevor Kimble on 4/1/24.
+//
+
+import Foundation
+
+
+@Observable
+class Order: Codable{
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    
+    var extraFrosting = false
+    var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool{
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    var cost: Double{
+        var cost = Double(quantity) * 2
+        
+        cost += (Double(type) / 2)
+        
+        if extraFrosting{cost += Double(quantity)}
+        if addSprinkles{cost += Double(quantity) / 2}
+        
+        return cost
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zip = "zip"
+    }
+}
